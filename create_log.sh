6f55021da7dc27673fc969189fc17c0443f5086c
Lê Tân Ri <rilt@vng.com.vn>
2018-06-21 10:47:29 +0700

    tiny fix

--- create_log.sh
+++ create_log.sh
@@ -1,5 +1,5 @@
 FILE_NAME="/home/ri/meet_jenkins/log_"`date +%Y_%m_%d_%M`".log"
 DATE_NOW=`date +%Y/%m/%d-%H:%M:%S`
-echo "Creating txt file"
-echo "Created by RI. Created time is $DATE_NOW" >  $FILE_NAME
+echo "Creating file"
+echo "Created by RI Ahihi. Created time is $DATE_NOW" >  $FILE_NAME
 echo "Completed"
